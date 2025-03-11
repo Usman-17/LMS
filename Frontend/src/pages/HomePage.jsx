0f89065941cfd45fd20fd9f00d6ed5f9eaa00bfc
Usman-17 <usmandev580@gmail.com>
2025-03-11 22:04:01 +0500

    feat: Implement testimonials section

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Companies from '../components/Companies'
 import CourseSection from '../components/CourseSection'
+import Testimonials from '../components/Testimonials'
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Hero />
       <Companies />
       <CourseSection />
+      <Testimonials />
     </div>
   )
 }
